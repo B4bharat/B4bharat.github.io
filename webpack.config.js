e38e5a26756ef7f3a63462d1c90e8f70127aca64
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-14 19:56:13 +0530

    basic react setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
   entry: './client/index.js',
@@ -11,5 +17,6 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
